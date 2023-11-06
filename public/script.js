95dac89dcb78585af51d7e3b327332beed8197f7
Alden <aldenp@email.sc.edu>
2023-11-05 22:02:22 -0500

    Assignment 13 turn in

--- public/script.js
+++ public/script.js
@@ -11,13 +11,22 @@ const showTeas = async () => {
         
         let h3 = document.createElement("h3");
         section.append(h3);
-        h3.innerHTML = teas.name;
+        h3.innerHTML = teas.name + " tea";
         
         let img = document.createElement("img");
         section.append(img);
         img.src = "http://localhost:3000/" + teas.img;
         img.width = 300;
         img.height = 300;
+        
+        let p = document.createElement("p");
+        section.append(p);
+        p.innerHTML += "<b>Caffeine ranges:</b> " + teas.caffine + `<br>`;
+        p.innerHTML += "<b>Flavor:</b> " + teas.flavor + `<br>`;
+        p.innerHTML += "<b>Aroma:</b> " + teas.aroma + `<br>`;
+        p.innerHTML += "<b>Common varieties:</b> " + teas.varieties + `<br>`;
+
+        
     });
 };
 
--- server.js
+++ server.js
@@ -11,31 +11,41 @@ app.get("/api/teas", (req, res) => {
   teas[0] = {
     name: "Green",
     caffine: "35-70mg",
-    forms: "brick, bag, loose leaf, powdered(macha)",
+    flavor: "fresh, grassy, and vegetal",
+    aroma: "grassy, seaweed-like, or floral notes, with a slight earthiness",
+    varieties: "Sencha, Matcha, Dragonwell, Gunpowder, Jasmine",
     img: "images/green_tea.jpg",
   };
   teas[1] = {
     name: "Black",
     caffine: "60-90mg",
-    forms: "brick, bag, loose leaf",
+    flavor: "robust and bold",
+    aroma: "malty, fruity, or floral notes with a hint of caramel or honey",
+    varieties: "Assam, Earl Grey, Darjeeling, Ceylon, Keemun",
     img: "images/black_tea.jpg",
   };
   teas[2] = {
     name: "White",
     caffine: "30-55mg",
-    forms: "brick, bag, loose leaf",
+    flavor: "delicate, subtle, and slightly sweet flavor",
+    aroma: "fresh, floral, and sometimes fruity, with a light honey-like sweetness",
+    varieties: "Silver Needle (Bai Hao Yin Zhen), White Peony (Bai Mu Dan), Shou Mei",
     img: "images/white_tea.jpg",
   };
   teas[3] = {
     name: "Oolong",
     caffine: "50-75mg",
-    forms: "brick, bag, loose leaf",
+    flavor: "between green and black teas in terms of flavor, offering a wide spectrum of tastes",
+    aroma: "can vary significantly, with some having fruity or orchid-like notes, while others have roasted or woody undertones",
+    varieties: "Tie Guan Yin, Da Hong Pao, Dong Ding, Ali Shan",
     img: "images/oolong_tea.jpg",
   };
   teas[4] = {
     name: "Pu'er",
     caffine: "30-70mg",
-    forms: "cake, bag, loose leaf, powdered",
+    flavor: "Raw pu'er is often earthy and vegetal, while ripe pu'er is known for its mellow, earthy, and sometimes woody taste",
+    aroma: "earthy, sometimes musty or woody for ripe pu'er, and fresh, grassy, or floral for raw pu'er",
+    varieties: "Sheng Pu'er, Shou Pu'er, Aged Pu'er",
     img: "images/puer_tea.jpg",
   };
   res.json(teas);
